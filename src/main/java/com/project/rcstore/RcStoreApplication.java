dfd51ef21a82cac77429a1160510fc7b464ca71c
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-08 13:14:14 -0700

    initialize blank project

---
+++ src/main/java/com/project/rcstore/RcStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.rcstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/rcstore/RcStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rcstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
