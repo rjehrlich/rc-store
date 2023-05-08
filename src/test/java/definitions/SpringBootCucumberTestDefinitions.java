183c5432eab59a6553cccbebd87437c98767eb83
clair <clairlee07@yahoo.com>
2023-05-08 17:27:00 -0400

    added definitions

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -1,9 +1,31 @@
 package definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class SpringBootCucumberTestDefinitions {
     @Given("a list of products are available")
     public void aListOfProductsAreAvailable() {
+        
+    }
+
+    @When("i add a product to my productList")
+    public void iAddAProductToMyProductList() {
+        
+    }
+
+    @Then("the product is added")
+    public void theProductIsAdded() {
+        
+    }
+
+    @When("i remove product from my productList")
+    public void iRemoveProductFromMyProductList() {
+        
+    }
+
+    @Then("the product is removed")
+    public void theProductIsRemoved() {
     }
 }
