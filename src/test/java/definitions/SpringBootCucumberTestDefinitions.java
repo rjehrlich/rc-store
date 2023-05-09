d3c211babe0878f1a8f323bf0a74a2a7cd3dbaf8
clair <clairlee07@yahoo.com>
2023-05-09 11:56:28 -0400

    started coding cucumber test definitions

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -1,13 +1,26 @@
 package definitions;
 
+import com.project.rcstore.RcStoreApplication;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RcStoreApplication.class)
 public class SpringBootCucumberTestDefinitions {
+    private static final String BASE_URL = "http://localhost:";
+
+    @LocalServerPort
+   String port;
+
+    private static Response response;
+
     @Given("a list of products are available")
     public void aListOfProductsAreAvailable() {
-
         
     }
 
