e5e582098ab30d0a46eb754994b7c66cffe4a21c
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-09 07:36:55 -0700

    created userProfile model

--- src/main/java/com/project/rcstore/model/UserProfile.java
+++ src/main/java/com/project/rcstore/model/UserProfile.java
@@ -1,4 +1,61 @@
 package com.project.rcstore.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "profiles")
 public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    public UserProfile() {
+    }
+
+    public UserProfile(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
