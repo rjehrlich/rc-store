31f5ecefeccf24926e816353f2c4355013508869
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-09 07:29:29 -0700

    setup product model

--- src/main/java/com/project/rcstore/model/Product.java
+++ src/main/java/com/project/rcstore/model/Product.java
@@ -1,4 +1,88 @@
 package com.project.rcstore.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private Double price;
+
+    @Column
+    private String description;
+
+    @Column
+    private String brandName;
+
+    @Column
+    private String url;
+
+
+    public Product() {
+    }
+
+    public Product(Long id, Double price, String description, String brandName, String url) {
+        this.id = id;
+        this.price = price;
+        this.description = description;
+        this.brandName = brandName;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", brandName='" + brandName + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
