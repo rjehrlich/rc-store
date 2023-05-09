2574f611f5c36bf8bc30876d082eaa57713e99a2
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-09 07:33:37 -0700

    created User model

--- src/main/java/com/project/rcstore/model/User.java
+++ src/main/java/com/project/rcstore/model/User.java
@@ -1,4 +1,74 @@
 package com.project.rcstore.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String emailAddress;
+
+    @Column
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String emailAddress, String password) {
+        this.id = id;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
