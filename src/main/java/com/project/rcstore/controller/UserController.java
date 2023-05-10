89fcc17a7bc0b7e66e1947d27ae4d25e8aea034c
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-10 07:38:45 -0700

    added user controller register endpoint & user repository

--- src/main/java/com/project/rcstore/controller/UserController.java
+++ src/main/java/com/project/rcstore/controller/UserController.java
@@ -1,4 +1,37 @@
 package com.project.rcstore.controller;
 
+import com.project.rcstore.exception.InformationExistException;
+import com.project.rcstore.model.User;
+import com.project.rcstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/users/")
 public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // accepting userObject
+    // http://localhost:8080/auth/users/register
+    @PostMapping(path = "/register/")
+    public User createUser(@RequestBody User userObject) {
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("User email already exist");
+        }
+    }
+
+    // complete after JWT token added
+    // http://localhost:8080/auth/users/login
+    // @PostMapping(path = "/login/")
 }
--- src/main/java/com/project/rcstore/repository/UserRepository.java
+++ src/main/java/com/project/rcstore/repository/UserRepository.java
@@ -4,5 +4,9 @@ import com.project.rcstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //return if the user exists in the db or not
+    boolean existsByEmailAddress(String emailAddress);
 
+    //if I am able to successfully logged in, need a new User object
+    User findUserByEmailAddress(String emailAddress);
 }
