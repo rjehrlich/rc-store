8dd5a68168dab3f0213f56d6a2bef8b01c0d6e65
clair <clairlee07@yahoo.com>
2023-05-10 15:34:37 -0400

    userservice

--- src/main/java/com/project/rcstore/service/UserService.java
+++ src/main/java/com/project/rcstore/service/UserService.java
@@ -3,40 +3,42 @@ package com.project.rcstore.service;
 import com.project.rcstore.exception.InformationExistException;
 import com.project.rcstore.model.User;
 import com.project.rcstore.repository.UserRepository;
+import com.project.rcstore.request.LoginRequest;
+import com.project.rcstore.response.LoginResponse;
 import com.project.rcstore.security.JWTUtils;
 import com.project.rcstore.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private JWTUtils jwtUtils;
-    private AuthenticationManager authenticationManager;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
     private MyUserDetails myUserDetails;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, MyUserDetails myUserDetails, JWTUtils jwtUtils, AuthenticationManager authenticationManager) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager, @Lazy MyUserDetails myUserDetails) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.myUserDetails = myUserDetails;
         this.jwtUtils = jwtUtils;
         this.authenticationManager = authenticationManager;
-
+        this.myUserDetails = myUserDetails;
     }
 
-    @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public User createUser(User userObject) {
         if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
             return userRepository.save(userObject);
         } else {
             throw new InformationExistException("User email already exist");
@@ -48,4 +50,18 @@ public class UserService {
     }
 
     // add loginUser once JWT
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            final String JWT = jwtUtils.generateJwtToken(myUserDetails);
+            return ResponseEntity.ok(new LoginResponse(JWT));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new LoginResponse("Error : user name or password is incorrect"));
+        }
+
+    }
 }
